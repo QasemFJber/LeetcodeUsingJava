f78d96a959f6982eb92bcaaaa525184027204127
Mohamed Naser <100851080+mahmmedn19@users.noreply.github.com>
2022-07-18 13:57:53 +0200

    Time: 6 ms (96.83%), Space: 54.7 MB (92.71%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+   public boolean containsDuplicate(int[] nums) {
+
+        //boolean flag= true;
+        Set<Integer>set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
